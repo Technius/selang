dc60e56e60cb3789648bee25342b56cfd21dbee2
Bryan Tan <techniux@gmail.com>
2018-02-18 23:27:53 -0800

    Implement an "environment" to hold variables

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Monad.Trans.Except
+import Control.Monad.Trans.State
 import Data.List (intercalate)
 import System.Environment (getArgs)
 import Text.Megaparsec (runParserT, ParseError)
@@ -12,12 +13,12 @@ import Selang.Eval
 main :: IO ()
 main = do
   args <- getArgs
-  result <- runExceptT $ parseAndEval args
+  (result, _) <- runStateT (runExceptT $ parseAndEval args) defaultEnv
   case result of
     Left err -> putStrLn err
     Right expr -> print expr
 
-parseAndEval :: [String] -> ExceptT String IO Term
+parseAndEval :: [String] -> ExceptT String (StateT Env IO) Term
 parseAndEval args = do
   term <- (withExceptT show . ExceptT) $ runParserT parser "<console>" (intercalate " " args)
   res <- withExceptT show (eval term)
--- src/Selang/Eval.hs
+++ src/Selang/Eval.hs
@@ -1,15 +1,27 @@
 module Selang.Eval
   ( eval
+  , defaultEnv
+  , Env
   , EvalError
   , EvalResult
   ) where
 
+import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except
+import Control.Monad.Trans.State
+import Data.Map as Map
 import Selang.Ast
 
-data EvalError = TypeMismatch deriving (Show)
+data EvalError = TypeMismatch
+               | UnknownIdent String
+               deriving (Show)
 
-type EvalResult m = ExceptT EvalError m Term
+type EvalResult m = ExceptT EvalError (StateT Env m) Term
+
+type Env = Map String Term
+
+defaultEnv :: Env
+defaultEnv = Map.fromList [("hello", Val $ toAst "Hello world!")]
 
 eval :: (Monad m) => Term -> EvalResult m
 eval (Cond cond t f) = do
@@ -18,4 +30,9 @@ eval (Cond cond t f) = do
     Val (BoolVal x) ->
       pure $ if x then t else f
     _ -> throwE TypeMismatch
+eval (Ident name) = do
+  env <- lift get
+  case Map.lookup name env of
+    Just v -> pure v
+    _ -> throwE (UnknownIdent name)
 eval x = pure x
